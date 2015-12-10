6e745475646c5b813e2eacd88159ae0f447b0d19
Brandon Kopp <brandonkopp@yahoo.com>
2015-12-10 06:25:10 -0500

    Commit 8 files for EDA Course Project 1

---
+++ plot2.R
@@ -0,0 +1,28 @@
+#EXPLORATORY DATA ANALYSIS
+#COURSE PROJECT 1
+
+
+setwd("../../4. Exploratory Data Analysis/Assignment1")
+#GETTING THE DATA
+if (!file.exists("data")){
+  dir.create("data")
+}
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL,destfile="./data/power.zip", method="curl")
+
+dateDownloaded <- date()
+dateDownloaded
+
+#SUBSETTING AND FORMATTING THE DATA
+power <- read.table(unz("./data/power.zip", "household_power_consumption.txt"), header=T,sep=";", stringsAsFactors = FALSE,na.strings = "?")
+power <- power[power$Date == "1/2/2007" | power$Date == "2/2/2007",  ]
+power$DateTime <- as.POSIXct(paste(power$Date, power$Time), format="%d/%m/%Y %H:%M:%S")
+power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
+power$Time <- strptime(power$Time,"%H:%M:%S")
+
+#PLOT 2
+png("plot2.png")
+plot(x = power$DateTime, y = power$Global_active_power, type="l", xlab="", 
+     ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,32 @@
+#EXPLORATORY DATA ANALYSIS
+#COURSE PROJECT 1
+
+setwd("../../4. Exploratory Data Analysis/Assignment1")
+#GETTING THE DATA
+if (!file.exists("data")){
+  dir.create("data")
+}
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL,destfile="./data/power.zip", method="curl")
+
+dateDownloaded <- date()
+dateDownloaded
+
+#SUBSETTING AND FORMATTING THE DATA
+power <- read.table(unz("./data/power.zip", "household_power_consumption.txt"), header=T,sep=";", stringsAsFactors = FALSE,na.strings = "?")
+power <- power[power$Date == "1/2/2007" | power$Date == "2/2/2007",  ]
+power$DateTime <- as.POSIXct(paste(power$Date, power$Time), format="%d/%m/%Y %H:%M:%S")
+power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
+power$Time <- strptime(power$Time,"%H:%M:%S")
+
+#PLOT 3
+png("plot3.png")
+plot(x = power$DateTime, y = power$Global_active_power, type="n", xlab="", 
+     ylab="Energy sub-metering", ylim = c(0,38), )
+lines(x = power$DateTime, y = power$Sub_metering_1, col="black")
+lines(x = power$DateTime, y = power$Sub_metering_2, col="red")
+lines(x = power$DateTime, y = power$Sub_metering_3, col="blue")
+legend("topright",lty="solid", col = c("black","red","blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+dev.off()
